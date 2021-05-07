f321943cf0ae752ada4a71ea9d5a35131a02e931
Robert Waring <Robert.Waring@bjss.com>
2021-05-07 13:57:42 +0100

    feature(cvsb-19353): use the huge message lib to handle large sqs messages

--- src/functions/edhMarshaller.ts
+++ src/functions/edhMarshaller.ts
@@ -1,9 +1,17 @@
-import {Callback, Context, DynamoDBRecord, DynamoDBStreamEvent, Handler} from "aws-lambda";
-import {AWSError, SQS} from "aws-sdk";
-import {SQService} from "../services/SQService";
-import {PromiseResult} from "aws-sdk/lib/request";
-import {SendMessageResult} from "aws-sdk/clients/sqs";
-import {debugOnlyLog, getTargetQueueFromSourceARN} from "../utils/Utils";
+import {
+  Callback, Context, DynamoDBRecord, DynamoDBStreamEvent, Handler,
+} from 'aws-lambda';
+import { AWSError } from 'aws-sdk';
+import { PromiseResult } from 'aws-sdk/lib/request';
+import { SendMessageResult } from 'aws-sdk/clients/sqs';
+import { SqsService } from '../utils/sqs-huge-msg';
+import { SQService } from '../services/SQService';
+import { debugOnlyLog, getTargetQueueFromSourceARN } from '../utils/Utils';
+import { Configuration } from '../utils/Configuration';
+
+function filterRecordsWithoutEventSourceARN(record: DynamoDBRecord): record is DynamoDBRecordWithEventSourceArn {
+  return record.eventSourceARN !== undefined;
+}
 
 /**
  * λ function to process a DynamoDB stream of test results into a queue for certificate generation.
@@ -11,53 +19,67 @@ import {debugOnlyLog, getTargetQueueFromSourceARN} from "../utils/Utils";
  * @param context - λ Context
  * @param callback - callback function
  */
-const edhMarshaller: Handler = async (event: DynamoDBStreamEvent, context?: Context, callback?: Callback): Promise<void | Array<PromiseResult<SendMessageResult, AWSError>>> => {
-    if (!event) {
-        console.error("ERROR: event is not defined.");
-        return;
-    }
-    const records: DynamoDBRecord[] = event.Records as DynamoDBRecord[];
-    if (!records || !records.length) {
-        console.error("ERROR: No Records in event: ", event);
-        return;
-    }
+const edhMarshaller: Handler = async (event: DynamoDBStreamEvent, context?: Context, callback?: Callback): Promise<void|Array<PromiseResult<SendMessageResult, AWSError>>> => {
+  if (!event) {
+    console.error('ERROR: event is not defined.');
+    return;
+  }
+  const records: DynamoDBRecord[] = event.Records;
+  if (!records || !records.length) {
+    console.error('ERROR: No Records in event: ', event);
+    return;
+  }
 
-    debugOnlyLog("Records: ", records);
+  debugOnlyLog('Records: ', records);
+  const region = process.env.AWS_REGION;
+  const config: any = Configuration.getInstance().getConfig();
 
-    // Instantiate the Simple Queue Service
-    const sqService: SQService = new SQService(new SQS());
-    const sendMessagePromises: Array<Promise<PromiseResult<SendMessageResult, AWSError>>> = [];
+  if (!region) {
+    console.error('AWS_REGION envvar not available');
+    return;
+  }
 
-    for (const record of records) {
-        debugOnlyLog("Record: ", record);
+  // Instantiate the Simple Queue Service
+  const sqsHugeMessage = new SqsService({
+    region,
+    queueName: config.queueName,
+    s3EndpointUrl: config.s3.remote.params.endpoint,
+    s3Bucket: 'BUCKET_NAME',
+  });
+  const sqService: SQService = new SQService(sqsHugeMessage);
+  // const sendMessagePromises: Array<Promise<PromiseResult<SendMessageResult, AWSError>>> = [];
 
-        if (!record.eventSourceARN) {
-            continue;
-        }
+  const filteredRecords = records.filter(filterRecordsWithoutEventSourceARN);
 
-        debugOnlyLog("New image: ", record.dynamodb?.NewImage)
+  // for (const record of filteredRecords) {
+  const sendMessagePromises = filteredRecords.map((record): Promise<PromiseResult<SendMessageResult, AWSError>> => {
+    debugOnlyLog('Record: ', record);
+    debugOnlyLog('New image: ', record.dynamodb?.NewImage);
 
-        const targetQueue = getTargetQueueFromSourceARN(record.eventSourceARN!);
+    const targetQueue = getTargetQueueFromSourceARN(record.eventSourceARN);
 
-        debugOnlyLog("Target Queue", targetQueue);
+    debugOnlyLog('Target Queue', targetQueue);
 
-        sendMessagePromises.push(
-            sqService.sendMessage(
-                JSON.stringify(record),
-                targetQueue
-            )
-        );
-    }
+    return sqService.sendMessage(
+      JSON.stringify(record),
+      targetQueue,
+    );
+  });
 
-    return Promise.all(sendMessagePromises)
+
+  return Promise.all(sendMessagePromises)
     .catch((error: AWSError) => {
-        console.error(error);
-        console.log("records");
-        console.log(records);
-        if(error.code !== "InvalidParameterValue"){
-            throw error;
-        }
+      console.error(error);
+      console.log('records');
+      console.log(records);
+      if (error.code !== 'InvalidParameterValue') {
+        throw error;
+      }
     });
 };
 
-export {edhMarshaller};
+export { edhMarshaller };
+
+interface DynamoDBRecordWithEventSourceArn extends DynamoDBRecord {
+  eventSourceARN: string;
+}
---
+++ src/utils/sqs-huge-msg.ts
@@ -0,0 +1,163 @@
+
+import * as AWS from 'aws-sdk';
+import { PromiseResult } from 'aws-sdk/lib/request';
+import { v4 as uuid } from 'uuid';
+
+export interface SqsServiceOptions {
+  endpoint?: string;
+  region: string;
+  queueName: string;
+  maxMessageSize?: number;
+  s3EndpointUrl: string;
+  s3Bucket: string;
+}
+
+export enum SqsServiceMessage {
+  MAX_SQS_MESSAGE_SIZE = 256 * 1024
+}
+export class SqsService {
+  private endpoint?: string;
+
+  private region: string;
+
+  private queueName: string;
+
+  private maxMessageSize: number;
+
+  private s3EndpointUrl: string;
+
+  private s3Bucket: string;
+
+  constructor(options: SqsServiceOptions) {
+    this.endpoint = options.endpoint;
+    this.region = options.region;
+    this.queueName = options.queueName;
+    this.maxMessageSize = options.maxMessageSize || SqsServiceMessage.MAX_SQS_MESSAGE_SIZE;
+    this.s3EndpointUrl = options.s3EndpointUrl;
+    this.s3Bucket = options.s3Bucket;
+  }
+
+  private getInstance(): AWS.SQS {
+    const sqsConfig = {
+      region: this.region,
+      endpoint: this.endpoint,
+    };
+    if (!this.endpoint) {
+      delete sqsConfig.endpoint;
+    }
+    return new AWS.SQS(sqsConfig);
+  }
+
+  private getInstanceS3(): AWS.S3 {
+    const s3Config = {
+      s3ForcePathStyle: true,
+      signatureVersion: 'v2',
+      region: this.region,
+      endpoint: this.s3EndpointUrl,
+    };
+    return new AWS.S3(s3Config);
+  }
+
+  private async deleteMessage(queueName: string, message: any): Promise<void> {
+    const queueUrl = await this.getQueueUrl(queueName);
+
+    if (queueUrl === undefined) {
+      return;
+    }
+
+    await this.getInstance()
+      .deleteMessage({
+        QueueUrl: queueUrl,
+        ReceiptHandle: message.Messages[0].ReceiptHandle,
+      }).promise();
+  }
+
+  public async getQueueUrl(queueName: string): Promise<string|undefined> {
+    const { QueueUrl } = await this.getInstance()
+      .getQueueUrl({
+        QueueName: queueName || this.queueName,
+      }).promise();
+
+    return QueueUrl;
+  }
+
+  public async sendMessage(queueName: string, body: string): Promise<PromiseResult<AWS.SQS.SendMessageResult, AWS.AWSError>|void> {
+    const msgSize = Buffer.byteLength(body, 'utf-8');
+    const queueUrl = await this.getQueueUrl(queueName);
+
+    if (queueUrl === undefined) {
+      return;
+    }
+
+    if (msgSize < this.maxMessageSize) {
+      const messageConfig = {
+        QueueUrl: queueUrl,
+        MessageBody: JSON.stringify({
+          message: body,
+        }),
+      };
+
+      this.getInstance().sendMessage(messageConfig).promise();
+    }
+
+    const keyId = uuid();
+    const payloadId = `${keyId}.json`;
+
+    const responseBucket = await this.getInstanceS3().upload({
+      Bucket: this.s3Bucket,
+      Body: JSON.stringify({
+        message: body,
+      }),
+      Key: payloadId,
+    }).promise();
+
+    const messageConfig = {
+      QueueUrl: queueUrl,
+      MessageBody: JSON.stringify({
+        S3Payload: {
+          Id: payloadId,
+          Key: responseBucket.Key,
+          Location: responseBucket.Location,
+        },
+      }),
+    };
+
+    this.getInstance().sendMessage(messageConfig).promise();
+  }
+
+  public async getMessage(queueName: string): Promise<PromiseResult<AWS.SQS.ReceiveMessageResult, AWS.AWSError>|void> {
+    const queueUrl = await this.getQueueUrl(queueName);
+
+    if (queueUrl === undefined) {
+      return;
+    }
+
+    const message = await this.getInstance()
+      .receiveMessage({ QueueUrl: queueUrl }).promise();
+
+    const messages = message.Messages;
+
+    if (!messages || messages.length === 0) {
+      return;
+    }
+
+    const { Body } = messages[0];
+    if (Body !== undefined && JSON.parse(Body).S3Payload) {
+      const s3Object = await this.getInstanceS3()
+        .getObject({
+          Bucket: this.s3Bucket,
+          Key: JSON.parse(Body).S3Payload.Key,
+        })
+        .promise();
+
+      if (s3Object.Body === undefined) {
+        throw new Error('Body missing from S3 object');
+      }
+      messages[0].Body = s3Object.Body.toString();
+    }
+
+    await this.deleteMessage(queueName, message);
+
+    return message;
+  }
+}
