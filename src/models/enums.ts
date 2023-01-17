d51fcb4b1af5e47fd66b2124108a67e5951dbc87
Aaron Downing <aaron.downing@bjss.com>
2023-01-17 16:24:38 +0000

    feat(cb2-7050): improve lambda efficiency

--- src/models/enums.ts
+++ src/models/enums.ts
@@ -1,4 +1,11 @@
-export enum ERROR {
+enum ERROR {
   NO_UNIQUE_TARGET = 'Unable to determine unique target',
   NO_SQS_CONFIG = 'SQS config is not defined in the config file.',
 }
+
+enum EventSource {
+  TECHNICAL_RECORDS,
+  TEST_RESULTS,
+}
+
+export { EventSource, ERROR };
